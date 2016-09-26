072fc116b0e381900fa79454da3ff84c579f3055
nematode195 <mike.tallsmall@gmail.com>
2016-09-26 16:29:15 -0400

    modified webpack.config for es2015 and react module loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var path = require('path');
-
+var dir_js = path.resolve(__dirname, 'js');
 var config = {
   context: path.join(__dirname, 'js'),
   entry: [
@@ -9,15 +9,21 @@ var config = {
     path: path.join(__dirname, 'www'),
     filename: 'bundle.js',
   },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel'],
-      },
-    ],
-  },
+   module: {
+        loaders: [
+            {
+                loader: 'react-hot',
+                test: dir_js,
+            },
+            {
+                loader: 'babel-loader',
+                test: dir_js,
+                query: {
+                    presets: ['es2015', 'react'],
+                },
+            }
+        ]
+    },
   resolveLoader: {
     root: [
       path.join(__dirname, 'node_modules'),
