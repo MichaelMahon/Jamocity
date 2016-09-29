f0f93d102a1554a82359317a3497e01623581574
nematode195 <mike.tallsmall@gmail.com>
2016-09-28 21:28:08 -0400

    Trying to do the update of the Immutable List

--- js/reducers/searches.js
+++ js/reducers/searches.js
@@ -28,16 +28,74 @@ const init = List([]);
 //https://www.sitepoint.com/how-to-build-a-todo-app-using-react-redux-and-immutable-js/
 function searches(state = init, action) {
 
-  switch (action.type) {
-    case 'RECEIVE_EBAY_POSTS':
-      return state.push(fromJS(action.payload));
 
-    case 'RECEIVE_REVERB_POSTS':
-      return state.push(fromJS(action.payload));
+  var existing = state.filter(t => {
+    console.log('searchKey');
+    console.log('comparing ' + t.get('searchKey') + " " + action.payload.searchKey);
+    return(t.get('searchKey') === action.payload.searchKey)
+  });
+  console.log('existing');
+  console.log(existing.size);
 
-    default:
-      return state
+  if(existing.size == 0) {
+    switch (action.type) {
+
+
+      case 'RECEIVE_EBAY_POSTS':
+        return state.push(fromJS(action.payload));
+
+      case 'RECEIVE_REVERB_POSTS':
+        return state.push(fromJS(action.payload));
+
+      default:
+        return state
+    }
   }
+  else
+  {
+    switch (action.type) {
+      case 'RECEIVE_EBAY_POSTS':
+        //return state.map(t => {
+          //if(t.get('searchKey') === action.payload.searchKey) {
+
+
+            //return t.update('eBaysearchResults', eBaysearchResults => action.payload.eBaysearchResults);
+
+            return state.update(
+                state.findIndex(function(item) {
+                  return item.get("searchKey") === action.payload.searchKey ;
+                }), function(item) {
+                  return item.set("eBaysearchResults", action.payload.eBaysearchResults);
+                }
+            );
+            return list;
+
+
+
+          //} else {
+          //  return t;
+         // }
+        //});
+
+
+      // case 'RECEIVE_REVERB_POSTS':
+      //   return state.map(t => {
+      //     //if(t.get('searchKey') === action.payload.searchKey) {
+      //       //return t.update('reverbsearchResults', reverbsearchResults => action.payload.reverbsearchResults);
+      //     //} else {
+      //       return t;
+      //     //}
+      //   });
+
+
+      default:
+        return state
+    }
+
+
+
+  }
+
 }
 
 
